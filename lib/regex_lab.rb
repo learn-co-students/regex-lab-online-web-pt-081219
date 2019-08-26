8e3e42a216c57475d0f31ebcb9684f978bbd4de9
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-26 19:34:54 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  word.match?(/\b[AEIOUaeiou]\w*/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
+  text[0].match?(/[A-Z]/) && text[-1].match?(/\W/)
+ end
 
 def valid_phone_number?(phone)
-
+  phone.match?(/\W?[0-9]{3}\W?[0-9]{3}\W?[0-9]{4}/)
 end
