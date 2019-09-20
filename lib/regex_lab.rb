74ce6ff2d3a856b0bc5bff2abff6a7ed88ab1576
LUIS J Alvarez Diaz <ldiaz244@gmail.com>
2019-09-20 03:16:02 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,18 @@
 def starts_with_a_vowel?(word)
-
+"aeiou".scan
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+"understanding".scan
 end
 
 def words_five_letters_long(text)
-
+"Apple".scan
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
+'El Barto!!'.match
 
 def valid_phone_number?(phone)
-
+"408 799-3236".match
 end
